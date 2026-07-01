db924fd76195fcda5f541c7e9270b4b27119f970
mack2807 <vinaynvs9@gmail.com>
2026-07-01 09:38:25 +0530

    Rotation of Array by d place with brute force approch

--- src/Array/Easy/Rotate_Array_By_D_Places.java
+++ src/Array/Easy/Rotate_Array_By_D_Places.java
@@ -1,5 +1,7 @@
 package Array.Easy;
 
+import java.util.Arrays;
+
 public class Rotate_Array_By_D_Places {
     //Right rotate
     //Optimal approch
@@ -24,10 +26,60 @@ public class Rotate_Array_By_D_Places {
         reverse(nums, 0, n - 1);
     }
 
+    //Brute Force Approach
+    // TC: O(n)
+    // SC: O(k)
+
+    // 1. Rotate the array to the left by k positions
+    public static void rotateLeft(int[] arr, int k){
+        int n = arr.length;
+        if (n == 0 ) return;
+        k = k % n;
+
+        //Store first k element in temp array
+        int[] temp = Arrays.copyOfRange(arr, 0 , k);
+
+        //Shift remaining element to left
+        for (int i = k; i< n; i++){
+            arr[i-k] = arr[i];
+        }
+
+        //Copy stored array to end
+        // System.arraycopy(temp, 0, arr, n - k, k);
+        for (int i = 0; i<k; i++){
+            arr[n-k+i] = temp[i];
+        }
+    }
+
+    // 2. Rotate the array to the right by k positions
+    public static void rotateRight(int[] arr, int k){
+        int n = arr.length;
+        if(n == 0) return;
+        k = k % n;
+        // Store last k elements
+        int[] temp = Arrays.copyOfRange(arr, n-k, n);
+
+        //Shift remaining element to right
+        for (int i = n-k-1; i>=0; i--){
+            arr[i+k] = arr[i];
+        }
+
+        // Copy the stored elements to the front
+        for (int i = 0; i < k; i++){
+            arr[i] = temp[i];
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int k = 3;
-        rotate(arr, k);
+//        rotate(arr, k);
+//        rotateLeft(arr, k);
+        rotateRight(arr, k);
+
 
         for (int num : arr){
             System.out.print(num + " ");
